20325833ed1ed4c0f381d9397d5b97fbde955fcf
LIKATHY <LIKATHY@lizhangkathy.localdomain>
2017-11-19 16:39:29 -0500

    Passwords Work

--- mangalike/lib/mangalike/accounts/user.ex
+++ mangalike/lib/mangalike/accounts/user.ex
@@ -1,3 +1,4 @@
+# Attribute to Prof Nathaniel Tuck's class notes and code
 defmodule Mangalike.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
@@ -8,6 +9,9 @@ defmodule Mangalike.Accounts.User do
     field :email, :string
     field :name, :string
     field :username, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -15,7 +19,30 @@ defmodule Mangalike.Accounts.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:name, :username, :email])
-    |> validate_required([:name, :username, :email])
+    |> cast(attrs, [:name, :username, :email, :password, :password_confirmation])
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
+    |> validate_required([:email, :password_hash])
   end
+
+  def validate_password(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, password ->
+      case valid_password?(password) do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || msg}]
+      end
+    end)
+  end
+
+  def put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+ def put_pass_hash(changeset), do: changeset
+
+  def valid_password?(password) when byte_size(password) >= 8 do
+    {:ok, password}
+
+  end
+  def valid_password?(_), do: {:error, "The password is too short"}
 end
